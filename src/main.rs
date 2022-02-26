ab25040d5312a2d4e329d6baedd666513b294bee
ShintaroOba <oba.shintaro@isid.co.jp>
2022-02-26 23:59:46 +0900

    テキストから文字列を読み込む

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,15 @@
+use std::{fmt::Result, fs};
+
 fn main() {
-    println!("Hello, world!");
+    print!("Hello");
+    match read() {
+        Ok(()) => println!("OK"),
+        Err(_) => print!("NG"),
+    }
+}
+
+fn read() -> Result {
+    let content = fs::read_to_string("./dictionary/dictionary.txt");
+    println!("{:?}", content);
+    Ok(())
 }
