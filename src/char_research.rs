6584447cdee72cd893a3c1944bda8b57c6ace14d
ShintaroOba <oba.shintaro@isid.co.jp>
2022-03-01 22:30:53 +0900

    GuessとAnswerの値オブジェクト作成

--- src/char_research.rs
+++ src/char_research.rs
@@ -1,20 +1,41 @@
-use std::error::Error;
-
-
-
-/// 内部に正解を持つ値オブジェクト
+/// An value object that holds a correct answer inside.
 pub struct Answer {
-    internal_string: String
+    target_word: String,
 }
 
-impl Answer{
-    // 文字列の探索結果を返すメソッド
-    fn check(word: &String) -> bool{
-        true
+impl Answer {
+    // factory method
+    pub fn new(target_word: &str) -> Self {
+        Answer {
+            target_word: target_word.to_string(),
+        }
+    }
+
+    pub fn internal_word(&self) -> String {
+        self.target_word.clone()
+    }
+
+    // 探索を行うメソッド
+    pub fn research(&self) {
+        let word_array = self.internal_word();
+        println!("word_array: {}", word_array);
     }
 }
 
-// 探索を行うメソッド
-pub fn research() {
-    
+/// An value object that holds a player's guess word inside.
+pub struct Guess {
+    guess_word: String,
+}
+
+impl Guess {
+    // factory method
+    pub fn new(guess_word: &str) -> Self {
+        Guess  {
+            guess_word: guess_word.to_string(),
+        }
+    }
+
+    pub fn internal_word(&self) ->String {
+        self.guess_word.clone()
+    }
 }
--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,21 @@
+use char_research::Answer;
+
 mod char_research;
 mod reader;
 
 const WORD_LENGTH: i32 = 5;
 const MAX_ATTEMPTS: i32 = 6;
 fn main() {
-    // TODO: for文でMAX_ATTEMPS文ループ回す
+    /// TODO: ランダムに正解となる単語を決定する
+    /// TODO: for文でMAX_ATTEMPS文ループ回す
+    /// TODO: 
+
     let input = reader::read_from_stdin();
     let content = reader::read_from_txt().unwrap();
+
+
+    let answer = Answer::new("WORDLE");
+    answer.research();
+
+
 }
