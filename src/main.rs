734bfd403e81651f08899d0a761e4aee31553d3d
ShintaroOba <oba.shintaro@isid.co.jp>
2022-03-02 22:15:17 +0900

    リファクタ

--- src/main.rs
+++ src/main.rs
@@ -1,21 +1,33 @@
-use char_research::{Answer, Guess};
-
-mod char_research;
+use colored::{ColoredString, Colorize};
+use word::{Answer, Guess};
 mod reader;
+mod word;
 
 const WORD_LENGTH: i32 = 5;
 const MAX_ATTEMPTS: i32 = 6;
+
 fn main() {
     /// TODO: ランダムに正解となる単語を決定する
     /// TODO: for文でMAX_ATTEMPS文ループ回す
-    /// TODO: 
-
-    let input = reader::read_from_stdin();
+    //  let input = reader::read_from_stdin();
     let content = reader::read_from_txt().unwrap();
-
 
     let answer = Answer::new("WORDLE");
     let guess = Guess::new("GUESS");
-    char_research::assert(&guess, &answer);
+    word::assert(&guess, &answer);
 
+    // GREEN
+    print!("{}", " W ".on_truecolor(152, 216, 105).black());
+    print!("{}", " O ".on_truecolor(152, 216, 105).black());
+
+    // YELLOW
+    print!("{}", " R ".on_truecolor(247, 225, 150).black());
+    print!("{}", " D ".on_truecolor(247, 225, 150).black());
+    // GRAY
+    print!("{}", " L ".on_truecolor(111, 114, 121).white());
+    print!("{}", " E ".on_truecolor(111, 114, 121).white());
+}
+
+fn display(){
+    
 }
--- src/word.rs
+++ src/word.rs
@@ -1,3 +1,4 @@
+use colored::{Colorize, ColoredString};
 /// An value object that holds a correct answer inside.
 pub struct Answer {
     target_word: String,
@@ -13,6 +14,23 @@ impl Answer {
 
     pub fn internal_word(&self) -> String {
         self.target_word.clone()
+    }
+}
+
+pub enum Color {
+    GREEN,
+    YELLOW,
+    GRAY,
+}
+
+impl Color {
+    fn decorate_word(&self, str: &str) -> ColoredString{
+        match self{
+            GREEN =>str.on_truecolor(152, 216, 105).black(),
+            YELLOW => str.on_truecolor(247, 225, 150).black(),
+            GRAY =>str.on_truecolor(111, 114, 121).white()
+        }
+        
     }
 }
 
@@ -42,6 +60,4 @@ pub fn assert(guess_word: &Guess, target_word: &Answer) {
     for x in guess_word.chars() {
         println!("word_array: {:?}", x);
     }
-
-    
 }
