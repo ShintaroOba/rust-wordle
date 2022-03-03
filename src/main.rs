03bc80e6c7852f96cc79b8ea5aaf34d8c2e260dd
ShintaroOba <oba.shintaro@isid.co.jp>
2022-03-03 22:16:19 +0900

    それっぽくした

--- src/main.rs
+++ src/main.rs
@@ -3,30 +3,34 @@ use word::{Answer, Guess};
 mod reader;
 mod word;
 
-const WORD_LENGTH: i32 = 5;
+const WORD_LENGTH: usize = 5;
 const MAX_ATTEMPTS: i32 = 6;
 
 fn main() {
-    /// TODO: ランダムに正解となる単語を決定する
-    /// TODO: for文でMAX_ATTEMPS文ループ回す
-    let mut input = String::new();
+    let mut attemps = 0;
 
-    while {
-        // 改行コードをtrim
-        input = reader::read_from_stdin().trim().to_string();
-        input.len() != 5
-    } {
-        if input.len() != 5 {
-            println!("Guess word must be 5 characters.");
+    while attemps < MAX_ATTEMPTS {
+        //let input = stdin();
+        let mut input = String::new();
+        while {
+            // 改行コードをtrim
+            print!("Enter your guess word {}/{}: ", attemps + 1, MAX_ATTEMPTS);
+            input = reader::read_from_stdin().trim().to_string();
+            input.len() != WORD_LENGTH
+        } {
+            if input.len() != WORD_LENGTH {
+                println!("Guess word must be 5 characters.");
+            }
         }
+        let guess = Guess::new(&input);
+        let random_word = get_random_word();
+        let answer = Answer::new(&random_word);
+        string_play(&guess, &answer);
+        attemps += 1;
     }
-    let guess = Guess::new(&input);
+}
 
-    let random_word = get_random_word();
-
-    let answer = Answer::new(&random_word);
-    println!("Answer word is: {:?}", &answer);
-
+fn string_play(guess: &Guess, answer: &Answer) {
     let word_vec = word::assert(&guess, &answer);
     for (i, val) in word_vec.iter().enumerate() {
         // VOが保有する文字列からi文字目のcharを取得
@@ -34,9 +38,10 @@ fn main() {
         let colored_str = val.decorate_word(&char.to_string());
         print!("{}", colored_str); // dont remove
     }
+    println!("");// dont remove
 }
-
 fn get_random_word() -> String {
+    // read words from csv file
     let contents = reader::read_from_txt().unwrap();
     let contents_vec: Vec<String> = contents
         .rsplit(',')
